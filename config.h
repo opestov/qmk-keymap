05e47c281ee78f614d6a829f998aed962de0edc6
Oleg Pestov <ijk@dindilen.com>
2023-04-09 19:11:04 +0300

    take snapshot

---
+++ config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define TAPPING_TERM 180
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_INTERVAL       16
+#define MOUSEKEY_WHEEL_DELAY    0
+#define MOUSEKEY_MAX_SPEED      6
+#define MOUSEKEY_TIME_TO_MAX    64
---
+++ keymap.c
@@ -0,0 +1,104 @@
+#include QMK_KEYBOARD_H
+
+#define ___ KC_NO
+#define LAYOUT_LR(\
+     L00, L01, L02, L03, L04,\
+L1X, L10, L11, L12, L13, L14,\
+L2X, L20, L21, L22, L23, L24,\
+L3X, L30, L31, L32, L33, L34,\
+     LT0, LT1, LT2,\
+     R00, R01, R02, R03, R04,\
+     R10, R11, R12, R13, R14, R1X,\
+     R20, R21, R22, R23, R24, R2X,\
+     R30, R31, R32, R33, R34, R3X,\
+     RT0, RT1, RT2)\
+LAYOUT_ansi_84(\
+___, L00, L01, L02, L03, L04, ___, ___, R00, R01, R02, R03, R04, ___, ___, QK_BOOT,\
+L1X, L10, L11, L12, L13, L14, ___, R10, R11, R12, R13, R14, R1X, ___, ___,\
+L2X, L20, L21, L22, L23, L24, ___, R20, R21, R22, R23, R24, R2X, ___, ___,\
+L3X, L30, L31, L32, L33, L34, ___, R30, R31, R32, R33, R34, R3X, ___,\
+___, ___, LT0, LT1, LT2, ___, ___, RT0, RT1, RT2, ___, ___, ___, ___,\
+DT_PRNT, DT_UP, DT_DOWN, ___, ___, ___, ___, ___, ___, ___)
+
+
+// clang-format off
+enum layers{
+  BASE,
+  SYM,
+  WIN_BASE,
+  WIN_FN,
+  NAV,
+  MOUSE
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[BASE] = LAYOUT_LR(
+               KC_1,           KC_2,           KC_3,           KC_4,           KC_5,
+KC_TAB,        KC_QUOT,        KC_COMM,        KC_DOT,         KC_P,           KC_Y,
+KC_ESC,        LCTL_T(KC_A),   LSFT_T(KC_O),   LALT_T(KC_E),   LT(SYM,KC_U),   KC_I,
+DF(BASE),      KC_COLN,        ALGR_T(KC_Q),   KC_J,           KC_K,           KC_X,
+               KC_LGUI,        MO(NAV),        LT(NAV,KC_SPC),
+
+               KC_6,           KC_7,           KC_8,           KC_9,           KC_0,
+               KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           ___,
+               KC_D,           LT(SYM,KC_H),   LALT_T(KC_T),   LSFT_T(KC_N),   LCTL_T(KC_S),   KC_ENT,
+               KC_B,           KC_M,           KC_W,           ALGR_T(KC_V),   KC_Z,           ___,
+               LT(MOUSE,KC_BSPC),MO(MOUSE),    LGUI_T(KC_DEL)),
+
+[SYM] = LAYOUT_LR(
+               KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,
+KC_TAB,        KC_QUOT,        KC_COMM,        KC_DOT,         KC_DQUO,        KC_PERC,       
+KC_ESC,        KC_EXLM,        KC_MINS,        KC_PLUS,        KC_EQL,         KC_UNDS,
+___,           KC_COLN,        KC_LABK,        KC_RABK,        KC_DLR,         KC_AT,
+               ___,            ___,            KC_SPC,
+
+               KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,
+               KC_CIRC,        KC_HASH,        KC_LPRN,        KC_RPRN,        KC_BSLS,        ___,
+               KC_AMPR,        KC_SCLN,        KC_LBRC,        KC_RBRC,        KC_QUES,        KC_ENT,
+               KC_PIPE,        KC_ASTR,        KC_LCBR,        KC_RCBR,        KC_SLSH,        ___,
+               KC_BSPC,        ___,            KC_DEL),
+
+[WIN_BASE] = LAYOUT_ansi_84(
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+[WIN_FN] = LAYOUT_ansi_84(
+     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+
+[NAV] = LAYOUT_LR(
+               ___,            ___,            ___,            ___,            ___,
+KC_ESC,        ___,            ___,            ___,            ___,            ___,
+KC_TAB,        KC_LCTL,        KC_LSFT,        KC_LALT,        ___,            ___,
+___,           ___,            KC_ALGR,        ___,            ___,            ___,
+               KC_LGUI,        ___,            KC_SPC,
+
+               ___,            ___,            ___,            ___,            ___,
+               ___,            KC_HOME,        KC_UP,          KC_END,         KC_PGUP,        ___,
+               CW_TOGG,        KC_LEFT,        KC_DOWN,        KC_RGHT,        KC_PGDN,        KC_ENT,
+               ___,            KC_PSCR,        ___,            KC_INS,         ___,            ___,
+               KC_BSPC,        ___,            KC_DEL),
+
+[MOUSE] = LAYOUT_LR(
+               KC_MPRV,        KC_MPLY,        KC_MNXT,        KC_MUTE,        KC_VOLU,
+___,           KC_WH_U,        KC_WH_L,        KC_MS_U,        KC_WH_R,        KC_VOLD,
+___,           KC_WH_D,        KC_MS_L,        KC_MS_D,        KC_MS_R,        ___,
+___,           ___,            ___,            ___,            ___,            ___,
+               KC_LGUI,        KC_BTN2,        KC_BTN1,
+
+               ___,            ___,            ___,            ___,            ___,
+               ___,            ___,            ___,            ___,            ___,            ___,
+               ___,            KC_LCTL,        KC_LSFT,        KC_LALT,        ___,            ___,
+               ___,            ___,            ___,            KC_ALGR,        KC_LGUI,        ___,
+               ___,            ___,            KC_LGUI)
+
+};
