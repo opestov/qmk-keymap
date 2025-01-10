5989776c04ba8996bf66624692ff54dad21f643f
Oleg Pestov <abc@defijk.com>
2025-01-10 07:45:40 +0500

    take snapshot

--- keymap.c
+++ keymap.c
@@ -37,7 +37,7 @@ enum custom_keycodes {
 LAYOUT_ansi_84(\
 L11,L12,L13,L14,L15,L16,xxx,xxx,R11,R12,R13,R14,R15,R16,KBD_TOGGLE,xxx,\
 L21,L22,L23,L24,L25,L26,xxx,xxx,R21,R22,R23,R24,R25,R26,xxx,\
-L31,L32,L33,L34,L35,KC_TAB,xxx,KC_DEL,R31,R32,R33,R34,R35,xxx,KC_PGUP,\
+L31,L32,L33,L34,L35,KC_ESC,xxx,KC_DEL,R31,R32,R33,R34,R35,xxx,KC_PGUP,\
 xxx,xxx,xxx,xxx,xxx,xxx,xxx,xxx,xxx,xxx,xxx,xxx,xxx,KC_PGDN,\
 xxx,xxx,xxx,LGUI_T(KC_HOME),KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,LGUI_T(KC_END),xxx,xxx,KC_MS_U,KC_BTN2,KC_WH_U,\
 xxx,xxx,xxx,LT(DIG,KC_SPC),xxx,KC_BTN1,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D)
@@ -70,12 +70,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [BASE] = LAYOUT_LR(
-    KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,
     KC_BSPC,        LCTL_T(KC_A),   LSFT_T(KC_S),   KC_D,           LT(SYM,KC_F),   KC_G,
                     LALT_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,
     KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_LBRC,
     KC_H,           LT(SYM,KC_J),   KC_K,           LSFT_T(KC_L),   LCTL_T(KC_SCLN),KC_ENT,
-    KC_N,           KC_M,           KC_COMM,        KC_DOT,         LALT_T(KC_QUOT)),
+    KC_N,           KC_M,           KC_F18,         KC_F19,         LALT_T(KC_F20)),
 
 [BASER] = LAYOUT_LR(
     _______,        _______,        _______,        _______,        _______,        _______,
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     _______,        _______,        _______,        _______,        _______,
     _______,        _______,        _______,        _______,        _______,        _______,
     _______,        LT(SYMR,KC_J),  _______,        _______,        LCTL_T(KC_SCLN),_______,
-    _______,        _______,        KC_COMM,        KC_DOT,         _______),
+    _______,        _______,        KC_COMM,        KC_DOT,         LALT_T(KC_QUOT)),
 
 [SYM] = LAYOUT_LR(
     KC_TILD,        KC_PIPE,        KC_LABK,        KC_RABK,        KC_DLR,         xxx,
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_MUTE,        KC_VOLD,        KC_VOLU,        KC_PSCR,        KC_INS,
     KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           xxx,
     KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    HYPR(KC_1),     HYPR(KC_2),     HYPR(KC_3),     HYPR(KC_4),     HYPR(KC_5)),
+    KC_F16,         KC_F17,         KC_F18,         KC_F19,         KC_F20),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
